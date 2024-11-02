93176631980964e12b082e45515b9ad80e13f278
Gonzalo <158757674+gcarpen480@users.noreply.github.com>
2024-11-02 20:58:19 +0100

    Add files via upload

---
+++ src/main/java/com/example/fragmentaciontabletlayout/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.fragmentaciontabletlayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager vp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void mostrarDatosIntroducidos(String nombre, String cantidad) {
+
+        BlankFragment4 fragmento = (BlankFragment4) ((ViewPagerAdapter) vp.getAdapter()).getItem(3);
+        fragmento.mostrarDato(nombre, cantidad);
+        vp.setCurrentItem(3);
+    }
+
+}
